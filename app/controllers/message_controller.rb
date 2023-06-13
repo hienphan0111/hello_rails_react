5d3bf413303d5d3a7397d639eac543d387125c74
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-06-14 00:19:37 +0700

    fix lint err

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,7 +1,7 @@
 class MessageController < ApplicationController
-    def message
-        num_rand = rand(1..5)
-        @message = Message.find(num_rand)
-        render json: @message
-    end
+  def message
+      num_rand = rand(1..5)
+      @message = Message.find(num_rand)
+      render json: @message
+  end
 end
